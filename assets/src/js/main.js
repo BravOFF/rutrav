bb86659bf78db4a39bd34e8bc8e299e89bc5f020
BravovRM <roman@bravov.ru>
2020-01-13 12:29:19 +0300

    Исправление ошибки в main.js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,8 +1,8 @@
 // Импортируем jQuery
-// = ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/jquery/dist/jquery.js
 
 // Импортируем Popper
-// = ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
 
 // Импортируем необходимые js-файлы Bootstrap 4
 //= ../../../node_modules/bootstrap/js/dist/util.js
